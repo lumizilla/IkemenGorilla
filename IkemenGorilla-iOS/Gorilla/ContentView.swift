97a522919db2ccbc69e897b2c92944296f092c1d
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-05-24 01:51:20 +0900

    create Gorilla project

---
+++ IkemenGorilla-iOS/Gorilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gorilla
+//
+//  Created by admin on 2020/05/24.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
