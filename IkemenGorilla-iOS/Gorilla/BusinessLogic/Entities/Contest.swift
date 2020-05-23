e515d2193a0a2d6116b14331febe524f7403b8cc
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-05-24 06:54:38 +0900

    add Contest Entity

---
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Entities/Contest.swift
@@ -0,0 +1,24 @@
+//
+//  Contest.swift
+//  Gorilla
+//
+//  Created by admin on 2020/05/24.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+
+struct Contest: Entity {
+    let id: String
+    let name: String
+    let start: Date
+    let end: Date
+    let status: String
+    let catchCopy: String
+    let description: String
+    let imageUrl: String
+    
+    static func == (lhs: Contest, rhs: Contest) -> Bool {
+        lhs.id == rhs.id
+    }
+}
