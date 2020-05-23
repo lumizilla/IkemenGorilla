418f3c8bfd8cac13a0fe210e65bbc2f1211decce
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-05-24 04:29:31 +0900

    fix SceneDelegate

--- IkemenGorilla-iOS/Gorilla/SceneDelegate.swift
+++ IkemenGorilla-iOS/Gorilla/SceneDelegate.swift
@@ -25,7 +25,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = DevelopingViewController()
+            window.rootViewController = TabBarController()
             self.window = window
             window.makeKeyAndVisible()
         }
