6284247fc0d73572922d3827bdd62d6f7ae9d59c
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-05-24 07:54:21 +0900

    fix HomeViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/Home/HomeViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/Home/HomeViewController.swift
@@ -6,4 +6,47 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class HomeViewController: UIViewController, ViewConstructor {
+    
+    // MARK: - Views
+    
+    private let scrollView = UIScrollView().then {
+        $0.alwaysBounceVertical = true
+    }
+    
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.alignment = .fill
+        $0.distribution = .fill
+    }
+    
+    private let currentContestsHeader = HomeCurrentContestHeader()
+    
+    // MARK: - Life Cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    
+    func setupViews() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
+        stackView.addArrangedSubview(currentContestsHeader)
+    }
+    
+    func setupViewConstraints() {
+        scrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        stackView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView)
+        }
+    }
+}
