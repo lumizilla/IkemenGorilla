f8dfc036dc8ebbfe3e15c32fd58957f680d2dae5
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-05-24 04:38:01 +0900

    fix DevelopingViewController argumat

--- IkemenGorilla-iOS/Gorilla/DevelopingViewController.swift
+++ IkemenGorilla-iOS/Gorilla/DevelopingViewController.swift
@@ -10,21 +10,44 @@ import UIKit
 import SnapKit
 import Then
 
-// Free to use to test your layout or something else ...
-final class DevelopingViewController: UIViewController {
+final class DevelopingViewController: UIViewController, ViewConstructor {
     
     private let titleLabel = UILabel().then {
         $0.text = "Developing ViewController"
     }
     
+    private let typeLabel = UILabel()
+    
+    init(type: String) {
+        super.init(nibName: nil, bundle: nil)
+        
+        typeLabel.text = type
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    func setupViews() {
         view.backgroundColor = .white
-        
         view.addSubview(titleLabel)
+        view.addSubview(typeLabel)
+    }
+    
+    func setupViewConstraints() {
         titleLabel.snp.makeConstraints {
             $0.center.equalToSuperview()
+        }
+        typeLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
         }
     }
 }
