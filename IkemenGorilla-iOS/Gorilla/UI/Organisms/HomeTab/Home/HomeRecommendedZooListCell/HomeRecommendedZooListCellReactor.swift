736ebf528688de4b887687f6136ede6d34cef11a
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-05-24 18:11:56 +0900

    fix HomeRecommendedZooListCellReactor

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/Home/HomeRecommendedZooListCell/HomeRecommendedZooListCellReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/Home/HomeRecommendedZooListCell/HomeRecommendedZooListCellReactor.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class HomeRecommendedZooListCellReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        let zoo: Zoo
+        
+        init(zoo: Zoo) {
+            self.zoo = zoo
+        }
+    }
+    
+    let initialState: HomeRecommendedZooListCellReactor.State
+    
+    init(zoo: Zoo) {
+        initialState = State(zoo: zoo)
+    }
+}
