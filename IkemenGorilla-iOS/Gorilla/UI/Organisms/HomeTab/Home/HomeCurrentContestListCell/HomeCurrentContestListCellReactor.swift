6376b876e13369203ebb1f1cd57fd53be15c1d46
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-05-24 15:02:38 +0900

    fix HomeCurrentContestListCellReactor

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/Home/HomeCurrentContestListCell/HomeCurrentContestListCellReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/Home/HomeCurrentContestListCell/HomeCurrentContestListCellReactor.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class HomeCurrentContestListCellReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        let contest: Contest
+        
+        init(contest: Contest) {
+            self.contest = contest
+        }
+    }
+    
+    let initialState: HomeCurrentContestListCellReactor.State
+    
+    init(contest: Contest) {
+        initialState = State(contest: contest)
+    }
+}
