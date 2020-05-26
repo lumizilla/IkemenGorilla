552493fac1ee0ad75d9ff6e16a3559aebe458384
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-05-26 14:29:51 +0900

    fix FrontendEchoTarget

--- IkemenGorilla-iOS/Gorilla/BusinessLogic/Networkings/Targets/FrontendEchoTarget.swift
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Networkings/Targets/FrontendEchoTarget.swift
@@ -29,18 +29,27 @@ extension FrontendEchoTarget: TargetType {
     
     var task: Task {
         switch self {
-        case .getEcho(let echo):
-            let parameters = ["echo": echo]
-            return .requestParameters(parameters: parameters, encoding: JSONEncoding.default)
+        case .getEcho(let text):
+            let parameters = [
+                "echo": text
+            ]
+            return .requestParameters(parameters: parameters, encoding: parameterEncoding)
+        }
+    }
+    
+    var parameterEncoding: ParameterEncoding {
+        switch self {
+            case .getEcho(_):
+                return URLEncoding.queryString
         }
     }
     
     var baseURL: URL {
-        return URL(string: "https://testlumi.herokuapp.com/") ?? undefined("endpoint for frontend echo dose not exist")
+        return URL(string: "https://testlumi.herokuapp.com") ?? undefined("endpoint for frontend echo dose not exist")
     }
     
     var headers: [String: String]? {
-        ["Content-Type": "application/json"]
+        nil
     }
 
     var sampleData: Data {
