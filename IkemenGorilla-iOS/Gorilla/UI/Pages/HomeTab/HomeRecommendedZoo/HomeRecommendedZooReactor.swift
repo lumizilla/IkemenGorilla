05992b786618aa9450c0ef9e4f56250fc2bced10
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-01 04:21:19 +0900

    update HomeRecommendedZooReactor

--- IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/HomeRecommendedZoo/HomeRecommendedZooReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/HomeRecommendedZoo/HomeRecommendedZooReactor.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class HomeRecommendedZooReactor: Reactor {
+    enum Action {
+        case load
+    }
+    
+    enum Mutation {
+        case setZooCellReactors([Zoo])
+        case setIsLoading(Bool)
+    }
+    
+    struct State {
+        var zooCellReactors: [HomeRecommendedZooCellReactor] = []
+        var isLoading: Bool = false
+    }
+    
+    let initialState = HomeRecommendedZooReactor.State()
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .load:
+            guard !currentState.isLoading else { return .empty() }
+            return .concat(
+                .just(.setIsLoading(true)),
+                load().map(Mutation.setZooCellReactors),
+                .just(.setIsLoading(false))
+            )
+        }
+    }
+    
+    private func load() -> Observable<[Zoo]> {
+        .just(TestData.testZoos(count: 8))
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case .setZooCellReactors(let zoos):
+            state.zooCellReactors = zoos.map { HomeRecommendedZooCellReactor(zoo: $0) }
+        case .setIsLoading(let isLoading):
+            state.isLoading = isLoading
+        }
+        return state
+    }
+}
