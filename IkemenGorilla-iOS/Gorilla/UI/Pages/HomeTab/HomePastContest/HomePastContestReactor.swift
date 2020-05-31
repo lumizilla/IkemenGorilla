9cf477d28bfc2f008ee305485355d1d8384b20d7
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-01 03:40:56 +0900

    update HomePastContestReactor

--- IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/HomePastContest/HomePastContestReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/HomePastContest/HomePastContestReactor.swift
@@ -10,8 +10,13 @@ import ReactorKit
 import RxSwift
 
 final class HomePastContestReactor: Reactor {
-    enum Action {}
-    enum Mutation {}
+    enum Action {
+        case load
+    }
+    enum Mutation {
+        case setContestCellReactors([Contest])
+        case setIsLoading(Bool)
+    }
     
     struct State {
         var contestCellReactors: [HomePastContestCellReactor] = []
@@ -19,4 +24,31 @@ final class HomePastContestReactor: Reactor {
     }
     
     let initialState = HomePastContestReactor.State()
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .load:
+            guard !currentState.isLoading else { return .empty() }
+            return Observable.concat(
+                .just(.setIsLoading(true)),
+                load().map(Mutation.setContestCellReactors),
+                .just(.setIsLoading(false))
+            )
+        }
+    }
+    
+    private func load() -> Observable<[Contest]> {
+        .just(TestData.testContests(count: 8))
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case .setContestCellReactors(let contests):
+            state.contestCellReactors = contests.map { HomePastContestCellReactor(contest: $0) }
+        case .setIsLoading(let isLoading):
+            state.isLoading = isLoading
+        }
+        return state
+    }
 }
