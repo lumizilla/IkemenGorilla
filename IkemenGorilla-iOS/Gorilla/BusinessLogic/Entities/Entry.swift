5b4152e035082105194c4db0c2675ded344fad04
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-01 17:28:09 +0900

    update Entry

--- IkemenGorilla-iOS/Gorilla/BusinessLogic/Entities/Entry.swift
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Entities/Entry.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+struct Entry: Entity {
+    let animalId: String
+    let name: String
+    let iconUrl: String
+    let zooName: String
+    
+    
+    static func == (lhs: Entry, rhs: Entry) -> Bool {
+        return lhs.animalId == rhs.animalId
+    }
+}
