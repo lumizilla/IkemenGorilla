9f174bac7747e862a8f41af8aebcf334b06dca28
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-01 18:17:10 +0900

    update ContestDetailPostCell

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestDetail/Post/ContestDetailPostCell.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestDetail/Post/ContestDetailPostCell.swift
@@ -6,4 +6,48 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class ContestDetailPostCell: UICollectionViewCell, View, ViewConstructor {
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        layer.masksToBounds = true
+        layer.cornerRadius = 0
+        addSubview(imageView)
+    }
+    
+    func setupViewConstraints() {
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: ContestDetailPostCellReactor) {
+        // Action
+        
+        // State
+    }
+}
