058bb893cd6b1948dd58fb47ed54afa1e4dcab58
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-02 00:35:22 +0900

    update ContestDetailInfoHeader

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestDetail/Info/ContestDetailInfoHeader.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestDetail/Info/ContestDetailInfoHeader.swift
@@ -26,6 +26,33 @@ final class ContestDetailInfoHeader: UIView, View, ViewConstructor {
         $0.numberOfLines = 0
     }
     
+    private let numberOfEntriesLabel = UILabel().then {
+        $0.apply(fontStyle: .medium, size: 13)
+        $0.textColor = Color.textBlack
+    }
+    
+    private let numberOfVotedPeopleLabel = UILabel().then {
+        $0.apply(fontStyle: .medium, size: 13)
+        $0.textColor = Color.textBlack
+    }
+    
+    private let numberOfVotesLabel = UILabel().then {
+        $0.apply(fontStyle: .medium, size: 13)
+        $0.textColor = Color.textBlack
+    }
+    
+    private let wayOfVoteLabel = UILabel().then {
+        $0.apply(fontStyle: .medium, size: 13)
+        $0.textColor = Color.textBlack
+        $0.text = "投票の方法：1人1日1票まで投票できます"
+    }
+    
+    private let sponsorHeaderLabel = UILabel().then {
+        $0.apply(fontStyle: .medium, size: 20)
+        $0.textColor = Color.black
+        $0.text = "SPONSOR"
+    }
+    
     // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -42,6 +69,11 @@ final class ContestDetailInfoHeader: UIView, View, ViewConstructor {
     func setupViews() {
         addSubview(catchCopyLabel)
         addSubview(descriptionLabel)
+        addSubview(numberOfEntriesLabel)
+        addSubview(numberOfVotedPeopleLabel)
+        addSubview(numberOfVotesLabel)
+        addSubview(wayOfVoteLabel)
+        addSubview(sponsorHeaderLabel)
     }
     
     func setupViewConstraints() {
@@ -53,7 +85,27 @@ final class ContestDetailInfoHeader: UIView, View, ViewConstructor {
             $0.top.equalTo(catchCopyLabel.snp.bottom).offset(24)
             $0.left.right.equalToSuperview()
             $0.width.equalTo(DeviceSize.screenWidth - 32)
-            $0.bottom.equalToSuperview()
+        }
+        numberOfEntriesLabel.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(16)
+            $0.left.equalToSuperview()
+        }
+        numberOfVotedPeopleLabel.snp.makeConstraints {
+            $0.top.equalTo(numberOfEntriesLabel.snp.bottom).offset(8)
+            $0.left.equalToSuperview()
+        }
+        numberOfVotesLabel.snp.makeConstraints {
+            $0.top.equalTo(numberOfVotedPeopleLabel.snp.bottom).offset(8)
+            $0.left.equalToSuperview()
+        }
+        wayOfVoteLabel.snp.makeConstraints {
+            $0.top.equalTo(numberOfVotesLabel.snp.bottom).offset(8)
+            $0.left.equalToSuperview()
+        }
+        sponsorHeaderLabel.snp.makeConstraints {
+            $0.top.equalTo(wayOfVoteLabel.snp.bottom).offset(36)
+            $0.left.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(16)
         }
     }
     
@@ -70,6 +122,24 @@ final class ContestDetailInfoHeader: UIView, View, ViewConstructor {
         reactor.state.map { $0.contest.description }
             .distinctUntilChanged()
             .bind(to: descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.contest.numberOfEntries }
+            .distinctUntilChanged()
+            .map { "エントリー数：\($0)" }
+            .bind(to: numberOfEntriesLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.contest.numberOfVotedPeople }
+            .distinctUntilChanged()
+            .map { "これまでの参加人数：\($0)" }
+            .bind(to: numberOfVotedPeopleLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.contest.numberOfVotes }
+            .distinctUntilChanged()
+            .map { "これまでの投票数：\($0)" }
+            .bind(to: numberOfVotesLabel.rx.text)
             .disposed(by: disposeBag)
     }
 }
