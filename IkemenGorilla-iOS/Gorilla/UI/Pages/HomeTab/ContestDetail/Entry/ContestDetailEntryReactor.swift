04cd02b523bfdccbb5b53817d29afa84c2f2bc29
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-01 17:53:19 +0900

    add load action to ContestDetailEntryReactor

--- IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/Entry/ContestDetailEntryReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/Entry/ContestDetailEntryReactor.swift
@@ -10,11 +10,18 @@ import ReactorKit
 import RxSwift
 
 final class ContestDetailEntryReactor: Reactor {
-    enum Action {}
-    enum Mutation {}
+    enum Action {
+        case load
+    }
+    enum Mutation {
+        case setEntryCellReactors([Entry])
+        case setIsLoading(Bool)
+    }
     
     struct State {
         let contest: Contest
+        var entryCellReactors: [ContestDetailEntryCellReactor] = []
+        var isLoading: Bool = false
         
         init(contest: Contest) {
             self.contest = contest
@@ -25,5 +32,32 @@ final class ContestDetailEntryReactor: Reactor {
     
     init(contest: Contest) {
         initialState = State(contest: contest)
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .load:
+            guard !currentState.isLoading else { return .empty() }
+            return .concat(
+                .just(.setIsLoading(true)),
+                load().map(Mutation.setEntryCellReactors),
+                .just(.setIsLoading(false))
+            )
+        }
+    }
+    
+    private func load() -> Observable<[Entry]> {
+        return .just(TestData.entries(count: 8))
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case .setEntryCellReactors(let entries):
+            state.entryCellReactors = entries.map { ContestDetailEntryCellReactor(entry: $0) }
+        case .setIsLoading(let isLoading):
+            state.isLoading = isLoading
+        }
+        return state
     }
 }
