c43b16857892932aecf22c1d79507f2979fb122b
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-01 23:05:41 +0900

    update ContestDetailResultReactor

--- IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/Result/ContestDetailResultReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/Result/ContestDetailResultReactor.swift
@@ -16,12 +16,14 @@ final class ContestDetailResultReactor: Reactor {
     
     enum Mutation {
         case setAwardCellReactors([Award])
+        case setResultCellReactors([ContestResult])
         case setIsLoading(Bool)
     }
     
     struct State {
         let contest: Contest
         var awardCellReactors: [ContestDetailResultAwardCellReactor] = []
+        var resultCellReactors: [ContestDetailResultVoteCellReactor] = []
         var isLoading: Bool = false
         
         init(contest: Contest) {
@@ -41,14 +43,19 @@ final class ContestDetailResultReactor: Reactor {
             guard !currentState.isLoading else { return .empty() }
             return .concat(
                 .just(.setIsLoading(true)),
-                load().map(Mutation.setAwardCellReactors),
+                loadAwards().map(Mutation.setAwardCellReactors),
+                loadContestResults().map(Mutation.setResultCellReactors),
                 .just(.setIsLoading(false))
             )
         }
     }
     
-    private func load() -> Observable<[Award]> {
+    private func loadAwards() -> Observable<[Award]> {
         return .just(TestData.awards(count: 8))
+    }
+    
+    private func loadContestResults() -> Observable<[ContestResult]> {
+        return .just(TestData.contestResults(count: 12))
     }
     
     func reduce(state: State, mutation: Mutation) -> State {
@@ -56,6 +63,8 @@ final class ContestDetailResultReactor: Reactor {
         switch mutation {
         case .setAwardCellReactors(let awards):
             state.awardCellReactors = awards.map { ContestDetailResultAwardCellReactor(award: $0) }
+        case .setResultCellReactors(let contestResults):
+            state.resultCellReactors = contestResults.map { ContestDetailResultVoteCellReactor(contestResult: $0) }
         case .setIsLoading(let isLoading):
             state.isLoading = isLoading
         }
