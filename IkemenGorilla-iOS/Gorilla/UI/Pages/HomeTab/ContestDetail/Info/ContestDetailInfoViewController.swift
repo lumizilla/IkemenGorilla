0d87fd1f42c6035b952251733f379625819e00c5
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-01 15:14:21 +0900

    update ContestDetailInfoViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/Info/ContestDetailInfoViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/Info/ContestDetailInfoViewController.swift
@@ -6,4 +6,58 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SegementSlide
+import ReactorKit
+import RxSwift
+
+final class ContestDetailInfoViewController: UIViewController, View, ViewConstructor, SegementSlideContentScrollViewDelegate {
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    @objc var scrollView: UIScrollView {
+        return contentScrollView
+    }
+    
+    // MARK: - Views
+    private let contentScrollView = UIScrollView().then {
+        $0.alwaysBounceVertical = true
+    }
+    
+    private let catchCopyLabel = UILabel().then {
+        $0.text = "ワイが一番イケメンやで"
+        $0.apply(fontStyle: .medium, size: 20)
+        $0.textColor = Color.black
+    }
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        view.addSubview(contentScrollView)
+        contentScrollView.addSubview(catchCopyLabel)
+    }
+    
+    func setupViewConstraints() {
+        contentScrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        catchCopyLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(24)
+            $0.left.right.equalToSuperview().inset(16)
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: ContestDetailInfoReactor) {
+        // Action
+        
+        // State
+    }
+}
