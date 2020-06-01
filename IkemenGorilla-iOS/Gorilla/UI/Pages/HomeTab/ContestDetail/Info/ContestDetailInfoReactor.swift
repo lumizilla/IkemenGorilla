207a5a6111e28d373adfb38ea1035c39e7c74893
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-01 14:35:12 +0900

    update ContestDetailInfoReactor

--- IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/Info/ContestDetailInfoReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/Info/ContestDetailInfoReactor.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class ContestDetailInfoReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        let contest: Contest
+        
+        init(contest: Contest) {
+            self.contest = contest
+        }
+    }
+    
+    let initialState: ContestDetailInfoReactor.State
+    
+    init(contest: Contest) {
+        initialState = State(contest: contest)
+    }
+}
