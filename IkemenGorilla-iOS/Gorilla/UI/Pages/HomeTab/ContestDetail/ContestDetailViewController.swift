963e0f98e358fd66b2570c47b9063eeaba0cb73f
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-01 15:14:36 +0900

    update ContestDetailViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/ContestDetailViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/ContestDetailViewController.swift
@@ -6,4 +6,57 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SegementSlide
+import ReactorKit
+import RxSwift
+
+final class ContestDetailViewController: SegementSlideDefaultViewController, View {
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let contestDetailHeader = ContestDetailHeader()
+    
+    // MARK: - Segement Settings
+    
+    override var titlesInSwitcher: [String] {
+        return ["情報", "エントリー", "投稿", "結果"]
+    }
+    
+    override func segementSlideHeaderView() -> UIView? {
+        return contestDetailHeader
+    }
+    
+    override var switcherConfig: SegementSlideDefaultSwitcherConfig {
+        var config = super.switcherConfig
+        config.normalTitleFont = TextStyle.font(.normalBold)()
+        config.selectedTitleFont = TextStyle.font(.normalBold)()
+        config.horizontalMargin = 24
+        config.horizontalSpace = 24
+        return config
+    }
+    
+    override func segementSlideContentViewController(at index: Int) -> SegementSlideContentScrollViewDelegate? {
+        return ContestDetailInfoViewController().then {
+            $0.reactor = ContestDetailInfoReactor(contest: TestData.testContest())
+        }
+    }
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        defaultSelectedIndex = 0
+        reloadData()
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: ContestDetailReactor) {
+        contestDetailHeader.reactor = reactor
+        
+        // Action
+        
+        // State
+    }
+}
