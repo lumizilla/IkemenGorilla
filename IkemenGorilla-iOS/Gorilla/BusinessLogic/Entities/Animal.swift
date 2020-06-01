5ada6a9875cf765cacdecad00904419a4663ca50
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-01 17:10:21 +0900

    update Animal

--- IkemenGorilla-iOS/Gorilla/BusinessLogic/Entities/Animal.swift
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Entities/Animal.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Animal: Entity {
+    let id: String
+    let name: String
+    let iconUrl: String
+    let sex: String
+    let birthday: Date
+    let description: String
+    let numberOfFans: Int
+    let isFan: Bool
+    let isVotedToday: Bool
+    
+    static func == (lhs: Animal, rhs: Animal) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
