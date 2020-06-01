bacea3ac6ecfc7eadbbd15a1e22654bef2a6b3d0
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-01 17:10:41 +0900

    update ContestDetailEntryAnimalCellReactor

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestDetail/Entry/ContestDetailEntryAnimalCellReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestDetail/Entry/ContestDetailEntryAnimalCellReactor.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class ContestDetailEntryAnimalCellReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        let animal: Animal
+        
+        init(animal: Animal) {
+            self.animal = animal
+        }
+    }
+    
+    let initialState: ContestDetailEntryAnimalCellReactor.State
+    
+    init(animal: Animal) {
+        initialState = State(animal: animal)
+    }
+}
