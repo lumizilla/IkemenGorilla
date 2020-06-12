10e27b96ff911788679e8d4091f7ff18efe279cb
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-13 01:01:42 +0900

    update header

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestAnimalDetail/ContestAnimalDetailHeader.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestAnimalDetail/ContestAnimalDetailHeader.swift
@@ -38,6 +38,29 @@ final class ContestAnimalDetailHeader: UIView, View, ViewConstructor {
         $0.textColor = Color.textBlack
     }
     
+    private let zooNameLabel = UILabel().then {
+        $0.apply(fontStyle: .bold, size: 15)
+        $0.textColor = Color.textBlack
+    }
+    
+    private let mapIconView = UIImageView().then {
+        $0.image = #imageLiteral(resourceName: "map_empty").withRenderingMode(.alwaysTemplate)
+        $0.tintColor = Color.textBlack
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private let addressLabel = UILabel().then {
+        $0.apply(fontStyle: .medium, size: 13)
+        $0.textColor = Color.textBlack
+        $0.adjustsFontSizeToFitWidth = true
+    }
+    
+    private let descriptionLabel = UILabel().then {
+        $0.apply(fontStyle: .bold, size: 13)
+        $0.textColor = Color.textGray
+        $0.numberOfLines = 0
+    }
+    
     private let voteButton = VoteButton()
     
     // MARK: - Initializers
@@ -58,6 +81,10 @@ final class ContestAnimalDetailHeader: UIView, View, ViewConstructor {
         addSubview(underlay)
         addSubview(animalNameLabel)
         addSubview(voteButton)
+        addSubview(zooNameLabel)
+        addSubview(mapIconView)
+        addSubview(addressLabel)
+        addSubview(descriptionLabel)
     }
     
     func setupViewConstraints() {
@@ -79,6 +106,25 @@ final class ContestAnimalDetailHeader: UIView, View, ViewConstructor {
             $0.centerY.equalTo(underlay)
             $0.width.equalTo(96)
         }
+        zooNameLabel.snp.makeConstraints {
+            $0.top.equalTo(underlay.snp.bottom)
+            $0.left.equalToSuperview().inset(16)
+        }
+        mapIconView.snp.makeConstraints {
+            $0.top.equalTo(zooNameLabel.snp.bottom).offset(8)
+            $0.left.equalToSuperview().inset(16)
+            $0.size.equalTo(16)
+        }
+        addressLabel.snp.makeConstraints {
+            $0.centerY.equalTo(mapIconView)
+            $0.left.equalTo(mapIconView.snp.right).offset(8)
+            $0.right.equalToSuperview().inset(16)
+        }
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(mapIconView.snp.bottom).offset(24)
+            $0.left.right.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().inset(24)
+        }
     }
     
     // MARK: - Bind Method
@@ -96,6 +142,21 @@ final class ContestAnimalDetailHeader: UIView, View, ViewConstructor {
         reactor.state.map { $0.entry.name }
             .distinctUntilChanged()
             .bind(to: animalNameLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.entry.zooName }
+            .distinctUntilChanged()
+            .bind(to: zooNameLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.response?.zooAddress }
+            .distinctUntilChanged()
+            .bind(to: addressLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.response?.description }
+            .distinctUntilChanged()
+            .bind(to: descriptionLabel.rx.text)
             .disposed(by: disposeBag)
     }
 }
