4b621dbee62f927cdc6cff317f629eff017b5505
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-12 21:55:01 +0900

    update ContestAnimalDetailPostCellReactor

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestAnimalDetail/ContestAnimalDetailPostCellReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestAnimalDetail/ContestAnimalDetailPostCellReactor.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class ContestAnimalDetailPostCellReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        let post: Post
+        
+        init(post: Post) {
+            self.post = post
+        }
+    }
+    
+    let initialState: ContestAnimalDetailPostCellReactor.State
+    
+    init(post: Post) {
+        initialState = State(post: post)
+    }
+}
+
+extension ContestAnimalDetailPostCellReactor: Equatable {
+    static func == (lhs: ContestAnimalDetailPostCellReactor, rhs: ContestAnimalDetailPostCellReactor) -> Bool {
+        return lhs.currentState.post.id == rhs.currentState.post.id
+    }
+}
