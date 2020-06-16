4ad515f49cfb1842f3447d69199f59f8abaa97fb
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-17 00:16:46 +0900

    update MapReactor

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/Map/MapReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/Map/MapReactor.swift
@@ -10,9 +10,35 @@ import ReactorKit
 import RxSwift
 
 final class MapReactor: Reactor {
-    enum Action {}
-    enum Mutation {}
-    struct State {}
+    enum Action {
+        case loadZoos
+    }
+    enum Mutation {
+        case setZoos([Zoo])
+    }
+    struct State {
+        var zoos: [Zoo] = []
+    }
     
     let initialState = State()
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .loadZoos:
+            return loadZoos().map(Mutation.setZoos)
+        }
+    }
+    
+    private func loadZoos() -> Observable<[Zoo]> {
+        return .just(TestData.zoos(count: 20))
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case .setZoos(let zoos):
+            state.zoos = zoos
+        }
+        return state
+    }
 }
