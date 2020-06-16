bcf3a3b351c918370a0b48715517dfc8ea318dac
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-16 23:50:03 +0900

    update MapViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/Map/MapViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/Map/MapViewController.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+import MapKit
+
+final class MapViewController: UIViewController, View, ViewConstructor {
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    let mapView = MKMapView()
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        view.addSubview(mapView)
+    }
+    
+    func setupViewConstraints() {
+        mapView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: MapReactor) {
+        // Action
+        
+        // State
+    }
+}
