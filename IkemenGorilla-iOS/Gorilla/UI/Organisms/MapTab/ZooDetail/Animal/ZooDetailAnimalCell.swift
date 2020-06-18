a7314fa7b38f7ebde3268de4f3e22fbf42adde5f
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-18 19:47:56 +0900

    udpate ZooDetailAnimalCell

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/ZooDetail/Animal/ZooDetailAnimalCell.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/ZooDetail/Animal/ZooDetailAnimalCell.swift
@@ -6,4 +6,55 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class ZooDetailAnimalCell: UICollectionViewCell, View, ViewConstructor {
+    struct Const {
+        static let itemSize: CGSize = CGSize(width: 96, height: 96)
+    }
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.layer.masksToBounds = true
+        $0.layer.cornerRadius = 48
+    }
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        addSubview(imageView)
+    }
+    
+    func setupViewConstraints() {
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: ZooDetailAnimalCellReactor) {
+        // Action
+        
+        // State
+        reactor.state.map { $0.animal.iconUrl }
+            .distinctUntilChanged()
+            .bind { [weak self] iconUrl in
+                self?.imageView.setImage(imageUrl: iconUrl)
+            }
+            .disposed(by: disposeBag)
+    }
+}
