35502940f7adaffa0e9e1aef44a3e0e39b994ce6
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-18 20:26:14 +0900

    add postsCollectionView

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooDetail/ZooDetailViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooDetail/ZooDetailViewController.swift
@@ -11,9 +11,13 @@ import RxSwift
 import ReusableKit
 
 final class ZooDetailViewController: UIViewController, View, ViewConstructor {
+    struct Const {
+        static let sectionHeight: CGFloat = ((DeviceSize.screenWidth - 48) / 3 + 8) * 6
+    }
     
     struct Reusable {
         static let animalCell = ReusableCell<ZooDetailAnimalCell>()
+        static let postCell = ReusableCell<ZooDetailPostCell>()
     }
     
     // MARK: - Variables
@@ -48,6 +52,14 @@ final class ZooDetailViewController: UIViewController, View, ViewConstructor {
     
     private let postsHeader = ZooDetailPostsHeader()
     
+    private lazy var postsCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout()).then {
+        $0.register(Reusable.postCell)
+        $0.backgroundColor = Color.white
+        $0.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        $0.contentInset.bottom = 16
+        $0.showsVerticalScrollIndicator = false
+    }
+    
     // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,6 +76,7 @@ final class ZooDetailViewController: UIViewController, View, ViewConstructor {
         stackView.addArrangedSubview(animalsHeader)
         stackView.addArrangedSubview(animalsCollectionView)
         stackView.addArrangedSubview(postsHeader)
+        stackView.addArrangedSubview(postsCollectionView)
     }
     
     func setupViewConstraints() {
@@ -82,17 +95,123 @@ final class ZooDetailViewController: UIViewController, View, ViewConstructor {
         }
     }
     
+    private func createLayout() -> UICollectionViewLayout {
+        let sideInset: CGFloat = 16
+        let insideInset: CGFloat = 8
+        let topInset: CGFloat = 8
+        let viewWidth: CGFloat = DeviceSize.screenWidth
+        let smallSquareWidth: CGFloat = (viewWidth - (sideInset * 2 + insideInset * 2)) / 3
+        let mediumSquareWidth: CGFloat = smallSquareWidth * 2 + insideInset
+        let nestedGroupHeight: CGFloat = mediumSquareWidth + topInset
+        let smallSquareGroupHeight: CGFloat = smallSquareWidth + topInset
+        
+        
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+
+            let nestedGroupTypeA: NSCollectionLayoutGroup = {
+                let smallSquareItem = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .absolute(smallSquareWidth + insideInset)))
+                smallSquareItem.contentInsets = NSDirectionalEdgeInsets(top: topInset, leading: 0, bottom: 0, trailing: insideInset)
+                let smallSquareGroup = NSCollectionLayoutGroup.vertical(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(smallSquareWidth + insideInset),
+                                                      heightDimension: .fractionalHeight(1.0)),
+                    subitem: smallSquareItem, count: 2)
+
+                let mediumSquareItem = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(mediumSquareWidth),
+                                                      heightDimension: .fractionalHeight(1.0)))
+                mediumSquareItem.contentInsets = NSDirectionalEdgeInsets(top: topInset, leading: 0, bottom: 0, trailing: 0)
+
+                let nestedGroup = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .absolute(nestedGroupHeight)),
+                    subitems: [smallSquareGroup, mediumSquareItem])
+                return nestedGroup
+            }()
+            
+            let nestedGroupTypeB: NSCollectionLayoutGroup = {
+                let mediumSquareItem = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(mediumSquareWidth + insideInset),
+                                                      heightDimension: .fractionalHeight(1.0)))
+                mediumSquareItem.contentInsets = NSDirectionalEdgeInsets(top: topInset, leading: 0, bottom: 0, trailing: insideInset)
+
+                let smallSquareItem = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .absolute(smallSquareWidth + insideInset)))
+                smallSquareItem.contentInsets = NSDirectionalEdgeInsets(top: topInset, leading: 0, bottom: 0, trailing: 0)
+                let smallSquareGroup = NSCollectionLayoutGroup.vertical(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(smallSquareWidth),
+                                                      heightDimension: .fractionalHeight(1.0)),
+                    subitem: smallSquareItem, count: 2)
+
+                let nestedGroup = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .absolute(nestedGroupHeight)),
+                    subitems: [mediumSquareItem, smallSquareGroup])
+                return nestedGroup
+            }()
+            
+            let nestedGroupTypeC: NSCollectionLayoutGroup = {
+                let smallSquareItem = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .absolute(smallSquareWidth),
+                                                      heightDimension: .fractionalHeight(1.0)))
+
+                let smallSquareGroup = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                      heightDimension: .absolute(smallSquareGroupHeight)),
+                    subitem: smallSquareItem,
+                    count: 3)
+                smallSquareGroup.interItemSpacing = .fixed(insideInset)
+                smallSquareGroup.contentInsets = NSDirectionalEdgeInsets(top: topInset, leading: 0, bottom: 0, trailing: 0)
+                
+                return smallSquareGroup
+            }()
+            
+            let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(nestedGroupHeight * 2 + smallSquareGroupHeight * 2)),
+            subitems: [nestedGroupTypeA, nestedGroupTypeC, nestedGroupTypeB, nestedGroupTypeC])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: sideInset, bottom: 0, trailing: sideInset)
+            return section
+
+        }
+        return layout
+    }
+    
     // MARK: - Bind Method
     func bind(reactor: ZooDetailReactor) {
         header.reactor = reactor
         // Action
         reactor.action.onNext(.loadAnimals)
+        reactor.action.onNext(.loadPosts)
         
         // State
         reactor.state.map { $0.animalCellReactors }
             .distinctUntilChanged()
             .bind(to: animalsCollectionView.rx.items(Reusable.animalCell)) { _, reactor, cell in
                 cell.reactor = reactor
+            }
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.postCellReactors }
+            .distinctUntilChanged()
+            .bind(to: postsCollectionView.rx.items(Reusable.postCell)) { _, reactor, cell in
+                cell.reactor = reactor
+            }
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.postCellReactors.count / 12 }
+            .distinctUntilChanged()
+            .bind { [weak self] sectionCount in
+                self?.stackView.removeConstraints(self?.postsCollectionView.constraints ?? [])
+                self?.postsCollectionView.snp.makeConstraints {
+                    $0.height.equalTo(16 + Const.sectionHeight * CGFloat(sectionCount))
+                    $0.width.equalTo(DeviceSize.screenWidth)
+                }
             }
             .disposed(by: disposeBag)
     }
