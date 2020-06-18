519af4da30d65d44ba5e09d9dbce69cf8bafc4eb
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-18 19:02:42 +0900

    add header

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooDetail/ZooDetailViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooDetail/ZooDetailViewController.swift
@@ -16,6 +16,7 @@ final class ZooDetailViewController: UIViewController, View, ViewConstructor {
     // MARK: - Views
     private let contentScrollView = UIScrollView().then {
         $0.showsVerticalScrollIndicator = false
+        $0.alwaysBounceVertical = true
     }
     
     private let stackView = UIStackView().then {
@@ -23,6 +24,8 @@ final class ZooDetailViewController: UIViewController, View, ViewConstructor {
         $0.alignment = .fill
         $0.distribution = .fill
     }
+    
+    private let header = ZooDetailHeader()
     
     // MARK: - Life Cycles
     override func viewDidLoad() {
@@ -34,15 +37,26 @@ final class ZooDetailViewController: UIViewController, View, ViewConstructor {
     
     // MARK: - Setup Methods
     func setupViews() {
-        
+        view.addSubview(contentScrollView)
+        contentScrollView.addSubview(stackView)
+        stackView.addArrangedSubview(header)
     }
     
     func setupViewConstraints() {
-        
+        contentScrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        header.snp.makeConstraints {
+            $0.width.equalTo(DeviceSize.screenWidth)
+        }
     }
     
     // MARK: - Bind Method
     func bind(reactor: ZooDetailReactor) {
+        header.reactor = reactor
         // Action
         
         // State
