0516cca0ba6dffd7fd6b6ec30cab20707958ff8e
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-19 02:22:42 +0900

    update ZooAnimalViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooAnimal/ZooAnimalViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooAnimal/ZooAnimalViewController.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class ZooAnimalViewController: UIViewController, View, ViewConstructor {
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        title = reactor?.currentState.zoo.name
+    }
+    
+    func setupViewConstraints() {
+        
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: ZooAnimalReactor) {
+        // Action
+        
+        // State
+    }
+}
