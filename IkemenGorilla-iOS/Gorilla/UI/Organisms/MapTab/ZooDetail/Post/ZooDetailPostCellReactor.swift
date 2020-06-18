bd85f0f4ec995cb3fcb536313396c86929046004
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-18 20:15:17 +0900

    update ZooDetailPostCellReactor

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/ZooDetail/Post/ZooDetailPostCellReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/ZooDetail/Post/ZooDetailPostCellReactor.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class ZooDetailPostCellReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        let post: Post
+        
+        init(post: Post) {
+            self.post = post
+        }
+    }
+    
+    let initialState: ZooDetailPostCellReactor.State
+    
+    init(post: Post) {
+        initialState = State(post: post)
+    }
+}
+
+extension ZooDetailPostCellReactor: Equatable {
+    static func == (lhs: ZooDetailPostCellReactor, rhs: ZooDetailPostCellReactor) -> Bool {
+        return lhs.currentState.post.id == rhs.currentState.post.id
+    }
+}
