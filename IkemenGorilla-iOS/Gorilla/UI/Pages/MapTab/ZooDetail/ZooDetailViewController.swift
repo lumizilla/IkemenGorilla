baec48b3d8bccd567279df69b12ca700bf163f63
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-18 18:42:37 +0900

    update ZooDetailViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooDetail/ZooDetailViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooDetail/ZooDetailViewController.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class ZooDetailViewController: UIViewController, View, ViewConstructor {
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let contentScrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+    }
+    
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.alignment = .fill
+        $0.distribution = .fill
+    }
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        
+    }
+    
+    func setupViewConstraints() {
+        
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: ZooDetailReactor) {
+        // Action
+        
+        // State
+    }
+}
