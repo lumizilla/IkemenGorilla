85e03a5b40fa7d82e2f717bd2ffc754008786bca
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-18 13:15:22 +0900

    update MapZooListViewController

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/Map/MapZooListViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/Map/MapZooListViewController.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class MapZooListViewController: UIViewController, View, ViewConstructor {
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        
+    }
+    
+    func setupViewConstraints() {
+        
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: MapReactor) {
+        // Action
+        
+        // State
+    }
+}
