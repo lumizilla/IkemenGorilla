175d096a09f3c0a0a404a00031259bd5958f0ffe
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-19 02:47:15 +0900

    add animalsCollectionView

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooAnimal/ZooAnimalViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooAnimal/ZooAnimalViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 import ReactorKit
 import RxSwift
+import ReusableKit
 
 final class ZooAnimalViewController: UIViewController, View, ViewConstructor {
+    
+    struct Reusable {
+        static let animalCell = ReusableCell<ZooAnimalCell>()
+    }
+    
     // MARK: - Variables
     var disposeBag = DisposeBag()
     
     // MARK: - Views
+    private let animalsCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
+        $0.itemSize = ZooAnimalCell.Const.itemSize
+        $0.minimumLineSpacing = 36
+        $0.minimumInteritemSpacing = 16
+    }).then {
+        $0.register(Reusable.animalCell)
+        $0.backgroundColor = Color.white
+        $0.showsVerticalScrollIndicator = false
+        $0.contentInset = UIEdgeInsets(top: 16, left: 24, bottom: 24, right: 24)
+    }
     
     // MARK: - Life Cycles
     override func viewDidLoad() {
@@ -27,16 +43,26 @@ final class ZooAnimalViewController: UIViewController, View, ViewConstructor {
     // MARK: - Setup Methods
     func setupViews() {
         title = reactor?.currentState.zoo.name
+        view.addSubview(animalsCollectionView)
     }
     
     func setupViewConstraints() {
-        
+        animalsCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
     
     // MARK: - Bind Method
     func bind(reactor: ZooAnimalReactor) {
         // Action
+        reactor.action.onNext(.loadAnimals)
         
         // State
+        reactor.state.map { $0.animalCellReactors }
+            .distinctUntilChanged()
+            .bind(to: animalsCollectionView.rx.items(Reusable.animalCell)) { _, reactor, cell in
+                cell.reactor = reactor
+            }
+            .disposed(by: disposeBag)
     }
 }
