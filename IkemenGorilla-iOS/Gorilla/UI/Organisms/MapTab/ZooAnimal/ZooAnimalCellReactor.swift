38d569b718805f295778680ef4fa9d644df02110
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-19 02:26:35 +0900

    update ZooAnimalCellReactor

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/ZooAnimal/ZooAnimalCellReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/ZooAnimal/ZooAnimalCellReactor.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class ZooAnimalCellReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        var animal: Animal
+        
+        init(animal: Animal) {
+            self.animal = animal
+        }
+    }
+    
+    let initialState: State
+    
+    init(animal: Animal) {
+        initialState = State(animal: animal)
+    }
+}
