e835b14e2eb0eb7e276fbc7ad94ae68ae6a29e3e
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-25 01:42:47 +0900

    update AnimalDetailViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/AnimalDetail/AnimalDetailViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/AnimalDetail/AnimalDetailViewController.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class AnimalDetailViewController: UIViewController, View, ViewConstructor {
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let contentScrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+        $0.alwaysBounceVertical = true
+    }
+    
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.alignment = .fill
+        $0.distribution = .fill
+    }
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        view.addSubview(contentScrollView)
+        contentScrollView.addSubview(stackView)
+    }
+    
+    func setupViewConstraints() {
+        contentScrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: AnimalDetailReactor) {
+        // Action
+        
+        // State
+    }
+}
