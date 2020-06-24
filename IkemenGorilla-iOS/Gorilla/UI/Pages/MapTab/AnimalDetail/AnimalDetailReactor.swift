113ab347a5103636ed96fdabff594c9930929aa1
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-25 01:38:35 +0900

    update AnimalDetailReactor

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/AnimalDetail/AnimalDetailReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/AnimalDetail/AnimalDetailReactor.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class AnimalDetailReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        let animal: Animal
+        
+        init(animal: Animal) {
+            self.animal = animal
+        }
+    }
+    
+    let initialState: State
+    
+    init(animal: Animal) {
+        initialState = State(animal: animal)
+    }
+}
