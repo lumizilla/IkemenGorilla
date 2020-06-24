5d3dfd4a55dd3f8f7eb1d1620086ca23800fb1d1
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-25 01:49:18 +0900

    update AnimalDetailHeader

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailHeader.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailHeader.swift
@@ -6,4 +6,61 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import ReactorKit
+
+final class AnimalDetailHeader: UIView, View, ViewConstructor {
+    
+    struct Const {
+        static let imageViewSize: CGFloat = 112
+    }
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.layer.masksToBounds = true
+        $0.layer.cornerRadius = Const.imageViewSize / 2
+    }
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        addSubview(imageView)
+    }
+    
+    func setupViewConstraints() {
+        imageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.left.equalToSuperview().inset(16)
+            $0.size.equalTo(Const.imageViewSize)
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: AnimalDetailReactor) {
+        // Action
+        
+        // State
+        reactor.state.map { $0.animal.iconUrl }
+            .distinctUntilChanged()
+            .bind { [weak self] iconUrl in
+                self?.imageView.setImage(imageUrl: iconUrl)
+            }
+            .disposed(by: disposeBag)
+    }
+}
