cebbdef9875dd26ee5dcba6bab5bc8b1d2688793
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-25 18:23:14 +0900

    update AnimalDetailPastContestView

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailPastContestView.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailPastContestView.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class AnimalDetailPastContestView: UIView, View, ViewConstructor {
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let pastContestLabel = UILabel().then {
+        $0.apply(fontStyle: .medium, size: 20)
+        $0.textColor = Color.textBlack
+        $0.text = "過去に参加したコンテスト"
+    }
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Views
+    func setupViews() {
+        addSubview(pastContestLabel)
+    }
+    
+    func setupViewConstraints() {
+        pastContestLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.left.right.equalToSuperview().inset(16)
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: AnimalDetailReactor) {
+        // Action
+        
+        // State
+    }
+}
