beba9f20240f5cfe5922afc778f8f4145d49bf53
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-25 17:41:59 +0900

    add floatingCurrentView

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailCurrentContestView.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailCurrentContestView.swift
@@ -22,6 +22,8 @@ final class AnimalDetailCurrentContestView: UIView, View, ViewConstructor {
         $0.text = "参加中のコンテスト"
     }
     
+    private let floatingContestView = FloatingContestView()
+    
     // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -37,11 +39,16 @@ final class AnimalDetailCurrentContestView: UIView, View, ViewConstructor {
     // MARK: - Setup Methods
     func setupViews() {
         addSubview(currentContestLabel)
+        addSubview(floatingContestView)
     }
     
     func setupViewConstraints() {
         currentContestLabel.snp.makeConstraints {
             $0.top.equalToSuperview()
+            $0.left.right.equalToSuperview().inset(16)
+        }
+        floatingContestView.snp.makeConstraints {
+            $0.top.equalTo(currentContestLabel.snp.bottom).offset(24)
             $0.left.right.equalToSuperview().inset(16)
         }
     }
@@ -51,5 +58,34 @@ final class AnimalDetailCurrentContestView: UIView, View, ViewConstructor {
         // Action
         
         // State
+        reactor.state.map { $0.currentContest.imageUrl }
+            .distinctUntilChanged()
+            .bind { [weak self] imageUrl in
+                self?.floatingContestView.imageView.setImage(imageUrl: imageUrl)
+            }
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.currentContest.name }
+            .distinctUntilChanged()
+            .bind(to: floatingContestView.contestNameLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map {
+                var durationString = "開催期間"
+                durationString += formatter.string(from: $0.currentContest.start)
+                durationString += " ~ "
+                durationString += formatter.string(from: $0.currentContest.end)
+                return durationString
+            }
+            .distinctUntilChanged()
+            .bind(to: floatingContestView.durationLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { $0.numberOfVoted }
+            .distinctUntilChanged()
+            .map { "現在\($0)人参加" }
+            .bind(to: floatingContestView.votedLabel.rx.text)
+            .disposed(by: disposeBag)
     }
 }
