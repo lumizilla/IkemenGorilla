d04c3ebdeb25036e8ac48adc80c67cf4652b69d8
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-26 02:27:02 +0900

    update AnimalDetailPastContestCellReactor

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailPastContestCell/AnimalDetailPastContestCellReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailPastContestCell/AnimalDetailPastContestCellReactor.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class AnimalDetailPastContestCellReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        let contest: Contest
+        
+        init(contest: Contest) {
+            self.contest = contest
+        }
+    }
+    
+    let initialState: AnimalDetailPastContestCellReactor.State
+    
+    init(contest: Contest) {
+        initialState = State(contest: contest)
+    }
+}
