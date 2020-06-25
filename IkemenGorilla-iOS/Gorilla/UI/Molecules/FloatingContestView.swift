4ccb72f307b565b6704dbe31d6e101924e47c944
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-25 17:40:30 +0900

    update FloatingContestView

--- IkemenGorilla-iOS/Gorilla/UI/Molecules/FloatingContestView.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Molecules/FloatingContestView.swift
@@ -12,7 +12,7 @@ final class FloatingContestView: UIView, ViewConstructor {
     
     
     // MARK: - Views
-    private let imageView = UIImageView().then {
+    let imageView = UIImageView().then {
         $0.contentMode = .scaleAspectFill
         $0.layer.masksToBounds = true
         $0.layer.cornerRadius = 8
@@ -22,6 +22,22 @@ final class FloatingContestView: UIView, ViewConstructor {
         $0.backgroundColor = Color.white
         $0.layer.masksToBounds = true
         $0.layer.cornerRadius = 8
+    }
+    
+    let contestNameLabel = UILabel().then {
+        $0.apply(fontStyle: .bold, size: 15)
+        $0.textColor = Color.textGray
+        $0.numberOfLines = 0
+    }
+    
+    let durationLabel = UILabel().then {
+        $0.apply(fontStyle: .bold, size: 10)
+        $0.textColor = Color.textGray
+    }
+    
+    let votedLabel = UILabel().then {
+        $0.apply(fontStyle: .bold, size: 13)
+        $0.textColor = Color.textGray
     }
     
     // MARK: - Initializers
@@ -38,8 +54,16 @@ final class FloatingContestView: UIView, ViewConstructor {
     
     // MARK: - Setup Methods
     func setupViews() {
-        addSubview(imageView)
         addSubview(sheet)
+        addSubview(imageView)
+        addSubview(contestNameLabel)
+        addSubview(durationLabel)
+        addSubview(votedLabel)
+        
+        layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.shadowColor = Color.black.cgColor
+        layer.shadowOpacity = 0.2
+        layer.shadowRadius = 8
     }
     
     func setupViewConstraints() {
@@ -50,7 +74,20 @@ final class FloatingContestView: UIView, ViewConstructor {
         sheet.snp.makeConstraints {
             $0.top.bottom.equalToSuperview().inset(8)
             $0.right.equalToSuperview()
-            $0.left.equalTo(imageView.snp.right)
+            $0.left.equalTo(imageView.snp.right).offset(-16)
+        }
+        contestNameLabel.snp.makeConstraints {
+            $0.top.right.equalTo(sheet).inset(16)
+            $0.left.equalTo(imageView.snp.right).offset(16)
+        }
+        durationLabel.snp.makeConstraints {
+            $0.left.equalTo(imageView.snp.right).offset(16)
+            $0.right.equalToSuperview().inset(16)
+            $0.bottom.equalTo(votedLabel.snp.top).offset(-8)
+        }
+        votedLabel.snp.makeConstraints {
+            $0.left.equalTo(imageView.snp.right).offset(16)
+            $0.right.bottom.equalToSuperview().inset(16)
         }
     }
 }
