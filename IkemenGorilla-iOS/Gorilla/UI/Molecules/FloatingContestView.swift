2c0f9678925ed348b9ce3adc6736da8d6ae16c61
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-25 15:59:34 +0900

    update FloatingContestView

--- IkemenGorilla-iOS/Gorilla/UI/Molecules/FloatingContestView.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Molecules/FloatingContestView.swift
@@ -6,4 +6,51 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class FloatingContestView: UIView, ViewConstructor {
+    
+    
+    // MARK: - Views
+    private let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.layer.masksToBounds = true
+        $0.layer.cornerRadius = 8
+    }
+    
+    private let sheet = UIView().then {
+        $0.backgroundColor = Color.white
+        $0.layer.masksToBounds = true
+        $0.layer.cornerRadius = 8
+    }
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        addSubview(imageView)
+        addSubview(sheet)
+    }
+    
+    func setupViewConstraints() {
+        imageView.snp.makeConstraints {
+            $0.top.left.bottom.equalToSuperview()
+            $0.size.equalTo(160)
+        }
+        sheet.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(8)
+            $0.right.equalToSuperview()
+            $0.left.equalTo(imageView.snp.right)
+        }
+    }
+}
