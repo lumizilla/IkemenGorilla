d09a3d824af81d4d646d6d32a7a0df640ec484aa
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-25 15:49:31 +0900

    update AnimalDetailCurrentContestView

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailCurrentContestView.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailCurrentContestView.swift
@@ -6,4 +6,50 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class AnimalDetailCurrentContestView: UIView, View, ViewConstructor {
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let currentContestLabel = UILabel().then {
+        $0.apply(fontStyle: .medium, size: 20)
+        $0.textColor = Color.textBlack
+        $0.text = "参加中のコンテスト"
+    }
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        addSubview(currentContestLabel)
+    }
+    
+    func setupViewConstraints() {
+        currentContestLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.left.right.equalToSuperview().inset(16)
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: AnimalDetailReactor) {
+        // Action
+        
+        // State
+    }
+}
