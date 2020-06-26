a2f68f32c04c593008fccbeb5b5c8ddb0ed7e870
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-26 18:02:17 +0900

    update PostCellReactor

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/Post/PostCellReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/Post/PostCellReactor.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class PostCellReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+    
+    struct State {
+        let post: Post
+        
+        init(post: Post) {
+            self.post = post
+        }
+    }
+    
+    let initialState: PostCellReactor.State
+    
+    init(post: Post) {
+        initialState = State(post: post)
+    }
+}
+
+extension PostCellReactor: Equatable {
+    static func == (lhs: PostCellReactor, rhs: PostCellReactor) -> Bool {
+        return lhs.currentState.post.id == rhs.currentState.post.id
+    }
+}
