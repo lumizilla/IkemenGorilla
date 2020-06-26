23382807e80b7c2f8b0c13ba50b4209055b5ce92
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-26 16:26:42 +0900

    add collectionview to AnimalDetailPastContestView

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailPastContestView.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailPastContestView.swift
@@ -9,16 +9,40 @@
 import UIKit
 import ReactorKit
 import RxSwift
+import ReusableKit
 
 final class AnimalDetailPastContestView: UIView, View, ViewConstructor {
+    struct Const {
+        static let height: CGFloat = 24 + 24 + AnimalDetailPastContestCell.Const.cellHeight + 24
+    }
+    
+    struct Reusable {
+        static let contestCell = ReusableCell<AnimalDetailPastContestCell>()
+    }
+    
     // MARK: - Variables
     var disposeBag = DisposeBag()
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: DeviceSize.screenWidth, height: Const.height)
+    }
     
     // MARK: - Views
     private let pastContestLabel = UILabel().then {
         $0.apply(fontStyle: .medium, size: 20)
         $0.textColor = Color.textBlack
         $0.text = "過去に参加したコンテスト"
+    }
+    
+    private let contestsCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
+        $0.itemSize = AnimalDetailPastContestCell.Const.itemSize
+        $0.minimumLineSpacing = 12
+        $0.scrollDirection = .horizontal
+    }).then {
+        $0.backgroundColor = Color.white
+        $0.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        $0.showsHorizontalScrollIndicator = false
+        $0.register(Reusable.contestCell)
     }
     
     // MARK: - Initializers
@@ -36,12 +60,20 @@ final class AnimalDetailPastContestView: UIView, View, ViewConstructor {
     // MARK: - Setup Views
     func setupViews() {
         addSubview(pastContestLabel)
+        addSubview(contestsCollectionView)
     }
     
     func setupViewConstraints() {
         pastContestLabel.snp.makeConstraints {
             $0.top.equalToSuperview()
-            $0.left.right.equalToSuperview().inset(16)
+            $0.left.equalToSuperview().inset(16)
+            $0.width.equalTo(DeviceSize.screenWidth - 32)
+            $0.height.equalTo(24)
+        }
+        contestsCollectionView.snp.makeConstraints {
+            $0.top.equalTo(pastContestLabel.snp.bottom).offset(24)
+            $0.width.equalTo(DeviceSize.screenWidth)
+            $0.height.equalTo(AnimalDetailPastContestCell.Const.cellHeight)
         }
     }
     
@@ -50,5 +82,11 @@ final class AnimalDetailPastContestView: UIView, View, ViewConstructor {
         // Action
         
         // State
+        reactor.state.map { $0.pastContestCellReactors }
+            .distinctUntilChanged()
+            .bind(to: contestsCollectionView.rx.items(Reusable.contestCell)) { _, reactor, cell in
+                cell.reactor = reactor
+            }
+            .disposed(by: disposeBag)
     }
 }
