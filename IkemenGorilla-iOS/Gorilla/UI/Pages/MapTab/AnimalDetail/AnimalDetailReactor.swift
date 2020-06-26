3cbe5ae6a97e4775f16dcd9cd5225ca53cd3168a
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-26 15:00:08 +0900

    add loadPastContest

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/AnimalDetail/AnimalDetailReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/AnimalDetail/AnimalDetailReactor.swift
@@ -10,14 +10,19 @@ import ReactorKit
 import RxSwift
 
 final class AnimalDetailReactor: Reactor {
-    enum Action {}
-    enum Mutation {}
+    enum Action {
+        case loadPastContests
+    }
+    enum Mutation {
+        case setPastContestCellReactors([Contest])
+    }
     
     struct State {
         let animal: Animal
         let zooName: String = "東山動物園"
         let currentContest: Contest = TestData.contest()
         let numberOfVoted: Int = 312
+        var pastContestCellReactors: [AnimalDetailPastContestCellReactor] = []
         
         init(animal: Animal) {
             self.animal = animal
@@ -28,5 +33,25 @@ final class AnimalDetailReactor: Reactor {
     
     init(animal: Animal) {
         initialState = State(animal: animal)
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .loadPastContests:
+            return loadPastContests().map(Mutation.setPastContestCellReactors)
+        }
+    }
+    
+    private func loadPastContests() -> Observable<[Contest]> {
+        return .just(TestData.contests(count: 5))
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case .setPastContestCellReactors(let contests):
+            state.pastContestCellReactors = contests.map { AnimalDetailPastContestCellReactor(contest: $0) }
+        }
+        return state
     }
 }
