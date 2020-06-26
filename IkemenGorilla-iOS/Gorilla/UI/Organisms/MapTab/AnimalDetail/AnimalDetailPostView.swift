3fd28d7ae667ee62525483065e408154b705dbc5
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-26 17:45:25 +0900

    update AnimalDetailPostView

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailPostView.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/MapTab/AnimalDetail/AnimalDetailPostView.swift
@@ -6,4 +6,51 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+import ReusableKit
+
+final class AnimalDetailPostView: UIView, View, ViewConstructor {
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let postsHeader = UILabel().then {
+        $0.apply(fontStyle: .medium, size: 20)
+        $0.textColor = Color.black
+        $0.text = "投稿"
+    }
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        addSubview(postsHeader)
+    }
+    
+    func setupViewConstraints() {
+        postsHeader.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.left.equalToSuperview().inset(16)
+            $0.width.equalTo(DeviceSize.screenWidth - 32)
+        }
+    }
+    
+    // Bind Method
+    func bind(reactor: AnimalDetailReactor) {
+        // Action
+        
+        // State
+    }
+}
