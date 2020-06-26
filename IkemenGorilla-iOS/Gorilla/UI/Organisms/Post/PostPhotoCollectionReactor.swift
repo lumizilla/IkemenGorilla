9fd72e5d06482d11bd1fb254941238572707d0d9
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-27 01:13:58 +0900

    update PostPhotoCollectionReactor

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/Post/PostPhotoCollectionReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/Post/PostPhotoCollectionReactor.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+class PostPhotoCollectionReactor: Reactor {
+    enum Action {
+        case setPosts([Post])
+    }
+    enum Mutation {
+        case setPostCellReactors([Post])
+    }
+    
+    struct State {
+        var postCellReactors: [PostCellReactor] = []
+    }
+    
+    let initialState = State()
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .setPosts(let posts):
+            return .just(.setPostCellReactors(posts))
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case .setPostCellReactors(let posts):
+            state.postCellReactors = posts.map { PostCellReactor(post: $0) }
+        }
+        return state
+    }
+}
