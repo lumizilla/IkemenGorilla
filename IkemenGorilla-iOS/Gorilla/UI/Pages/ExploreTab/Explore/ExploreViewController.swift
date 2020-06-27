42ad2739f02452d93fbd46b3f1805b001493c230
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-28 04:50:15 +0900

    update ExploreViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/ExploreTab/Explore/ExploreViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/ExploreTab/Explore/ExploreViewController.swift
@@ -6,4 +6,52 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+import ReusableKit
+
+final class ExploreViewController: UIViewController, View, ViewConstructor {
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let postsCollectionView = PostPhotoCollectionView().then {
+        $0.contentInset.top = 8
+    }
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        view.addSubview(postsCollectionView)
+    }
+    
+    func setupViewConstraints() {
+        postsCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - bind
+    func bind(reactor: ExploreReactor) {
+        postsCollectionView.reactor = PostPhotoCollectionReactor()
+        // Action
+        reactor.action.onNext(.loadPosts)
+        
+        // State
+        reactor.state.map { $0.posts }
+            .distinctUntilChanged()
+            .bind { [weak self] posts in
+                self?.postsCollectionView.reactor?.action.onNext(.setPosts(posts))
+            }
+            .disposed(by: disposeBag)
+    }
+}
