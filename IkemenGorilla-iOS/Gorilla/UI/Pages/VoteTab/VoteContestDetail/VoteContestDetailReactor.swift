22cfb90216c7e85919c9fb056a7cd2e35917d883
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-28 02:39:03 +0900

    update VoteContestDetailReactor

--- IkemenGorilla-iOS/Gorilla/UI/Pages/VoteTab/VoteContestDetail/VoteContestDetailReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/VoteTab/VoteContestDetail/VoteContestDetailReactor.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+
+final class VoteContestDetailReactor: Reactor {
+    enum Action {
+        case loadContests
+    }
+    enum Mutation {
+        case setEntryCellReactors([Entry])
+    }
+    
+    struct State {
+        let contest: Contest
+        var entryCellReactors: [ContestDetailEntryCellReactor] = []
+        
+        init(contest: Contest) {
+            self.contest = contest
+        }
+    }
+    
+    let initialState: State
+    
+    init(contest: Contest) {
+        initialState = State(contest: contest)
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .loadContests:
+            return loadContests().map(Mutation.setEntryCellReactors)
+        }
+    }
+    
+    private func loadContests() -> Observable<[Entry]> {
+        return .just(TestData.entries(count: 8))
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case .setEntryCellReactors(let entries):
+            state.entryCellReactors = entries.map { ContestDetailEntryCellReactor(entry: $0) }
+        }
+        return state
+    }
+}
