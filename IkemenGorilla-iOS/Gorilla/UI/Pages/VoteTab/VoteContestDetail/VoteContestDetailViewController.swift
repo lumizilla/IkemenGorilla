a1b126fa5284fa6ceb0baa540635f72ddc28c1b0
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-28 02:39:13 +0900

    update VoteContestViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/VoteTab/VoteContestDetail/VoteContestDetailViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/VoteTab/VoteContestDetail/VoteContestDetailViewController.swift
@@ -6,4 +6,102 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+import ReusableKit
+
+final class VoteContestDetailViewController: UIViewController, View, ViewConstructor {
+    
+    struct Const {
+        static let rowHeight: CGFloat = ContestDetailEntryCell.Const.cellHeight + 16
+    }
+    
+    struct Reusable {
+        static let entryCell = ReusableCell<ContestDetailEntryCell>()
+    }
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let contentScrollView = UIScrollView().then {
+        $0.showsVerticalScrollIndicator = false
+        $0.alwaysBounceVertical = true
+    }
+    
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.alignment = .fill
+        $0.distribution = .fill
+    }
+    
+    private let header = VoteContestDetailHeader()
+    
+    private let entriesCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
+        $0.itemSize = ContestDetailEntryCell.Const.itemSize
+        $0.minimumLineSpacing = 16
+        $0.minimumInteritemSpacing = 16
+    }).then {
+        $0.register(Reusable.entryCell)
+        $0.backgroundColor = Color.white
+        $0.showsVerticalScrollIndicator = false
+        $0.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        $0.alwaysBounceVertical = true
+    }
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        view.backgroundColor = Color.white
+        view.addSubview(contentScrollView)
+        contentScrollView.addSubview(stackView)
+        stackView.addArrangedSubview(header)
+        stackView.addArrangedSubview(entriesCollectionView)
+    }
+    
+    func setupViewConstraints() {
+        contentScrollView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        header.snp.makeConstraints {
+            $0.width.equalTo(DeviceSize.screenWidth)
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: VoteContestDetailReactor) {
+        header.reactor = reactor
+        
+        // Action
+        reactor.action.onNext(.loadContests)
+        
+        // State
+        reactor.state.map { $0.entryCellReactors }
+            .distinctUntilChanged()
+            .bind(to: entriesCollectionView.rx.items(Reusable.entryCell)) { _, reactor, cell in
+                cell.reactor = reactor
+            }
+            .disposed(by: disposeBag)
+        
+        reactor.state.map { ($0.entryCellReactors.count + 1) / 2 }
+        .distinctUntilChanged()
+            .bind { [weak self] rowCount in
+                logger.debug(rowCount)
+                self?.entriesCollectionView.removeConstraints(self?.entriesCollectionView.constraints ?? [])
+                self?.entriesCollectionView.snp.makeConstraints {
+                    $0.height.equalTo(CGFloat(rowCount) * Const.rowHeight)
+                }
+        }
+    }
+}
