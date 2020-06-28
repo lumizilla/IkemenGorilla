236070d49c76fbec81c5b0adc17c73bece02b1d2
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-28 13:23:27 +0900

    update MapSearchViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/MapSearch/MapSearchViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/MapSearch/MapSearchViewController.swift
@@ -6,4 +6,51 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class MapSearchViewController: UIViewController, View, ViewConstructor {
+    
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let searchBar = UISearchBar().then {
+        $0.placeholder = "動物園、住所"
+        $0.backgroundImage = UIImage()
+        $0.tintColor = Color.black
+        $0.showsCancelButton = true
+    }
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        view.backgroundColor = Color.white
+        navigationItem.titleView = searchBar
+        searchBar.becomeFirstResponder()
+    }
+    
+    func setupViewConstraints() {
+        
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: MapSearchReactor) {
+        // Action
+        searchBar.rx.cancelButtonClicked
+            .bind { [weak self] _ in
+                self?.dismiss(animated: true, completion: nil)
+            }
+            .disposed(by: disposeBag)
+        
+        // State
+    }
+}
