fb779e21fdb7ba07dfb0f61b1cecf3f0faaf8039
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-28 12:10:43 +0900

    update ExplorePostDetailViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/ExploreTab/ExplorePostDetail/ExplorePostDetailViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/ExploreTab/ExplorePostDetail/ExplorePostDetailViewController.swift
@@ -26,6 +26,11 @@ final class ExplorePostDetailViewController: UIViewController, View, ViewConstru
         $0.separatorStyle = .none
         $0.showsVerticalScrollIndicator = false
         $0.allowsSelection = false
+        $0.alpha = 0
+    }
+    
+    private let activityIndicator = UIActivityIndicatorView().then {
+        $0.startAnimating()
     }
     
     // MARK: - Life Cycles
@@ -36,14 +41,33 @@ final class ExplorePostDetailViewController: UIViewController, View, ViewConstru
         setupViewConstraints()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        guard let startAt = reactor?.currentState.startAt else { return }
+        guard let didScrollToItem = reactor?.currentState.didScrollToItem else { return }
+        if !didScrollToItem {
+            postsTableView.scrollToRow(at: IndexPath(row: startAt, section: 0), at: .top, animated: false)
+            reactor?.action.onNext(.didScrollToItem)
+        }
+        UIView.animate(withDuration: 0.2, animations: {
+            self.postsTableView.alpha = 1
+            self.activityIndicator.stopAnimating()
+        })
+    }
+    
     // MARK: - Setup Methods
     func setupViews() {
+        view.backgroundColor = Color.white
         view.addSubview(postsTableView)
+        view.addSubview(activityIndicator)
     }
     
     func setupViewConstraints() {
         postsTableView.snp.makeConstraints {
             $0.edges.equalToSuperview()
+        }
+        activityIndicator.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(120)
+            $0.centerX.equalToSuperview()
         }
     }
     
