1106fd272d37f1e70dad460baebd65fc075de635
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-30 02:46:34 +0900

    update constraints

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooDetail/ZooDetailViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooDetail/ZooDetailViewController.swift
@@ -219,7 +219,7 @@ final class ZooDetailViewController: UIViewController, View, ViewConstructor {
         reactor.state.map { $0.postCellReactors.count / 12 }
             .distinctUntilChanged()
             .bind { [weak self] sectionCount in
-                self?.stackView.removeConstraints(self?.postsCollectionView.constraints ?? [])
+                self?.postsCollectionView.removeConstraints(self?.postsCollectionView.constraints ?? [])
                 self?.postsCollectionView.snp.makeConstraints {
                     $0.height.equalTo(16 + Const.sectionHeight * CGFloat(sectionCount))
                     $0.width.equalTo(DeviceSize.screenWidth)
