f4a8a5d6bb8246f5aae701c49368bcee55759df2
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-30 01:03:29 +0900

    add animalService

--- IkemenGorilla-iOS/Gorilla/BusinessLogic/Service/ServiceProvider.swift
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Service/ServiceProvider.swift
@@ -8,11 +8,14 @@
 
 protocol ServiceProviderType: AnyObject {
     var contestService: ContestServiceType { get set }
+    var animalService: AnimalServiceType { get set }
 }
 
 final class ServiceProvider: ServiceProviderType {
     
     private lazy var contestRepository = ContestRepository(networkProvider: NetworkProvider<ContestTarget>())
+    private lazy var animalRepository = AnimalRepository(networkProvider: NetworkProvider<AnimalTarget>())
     
     lazy var contestService: ContestServiceType = ContestService(provider: self, contestRepository: contestRepository)
+    lazy var animalService: AnimalServiceType = AnimalService(provider: self, animalRepository: animalRepository)
 }
