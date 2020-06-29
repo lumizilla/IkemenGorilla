ba2eca49eee86c985483d7cf63b7dcb25db6c136
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-30 00:51:48 +0900

    update AnimalTarget

--- IkemenGorilla-iOS/Gorilla/BusinessLogic/Networkings/Targets/AnimalTarget.swift
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Networkings/Targets/AnimalTarget.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import Moya
+
+enum AnimalTarget {
+    case getPosts(animalId: String, page: Int)
+}
+
+extension AnimalTarget: TargetType {
+     var path: String {
+        switch self {
+        case .getPosts(let animalId, _):
+            return "/animals/\(animalId)/posts"
+        }
+    }
+    
+    var method: Method {
+        switch self {
+        case .getPosts:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .getPosts(_, let page):
+            let parameters = [
+                "page": page
+            ]
+            return .requestParameters(parameters: parameters, encoding: parameterEncoding)
+        }
+    }
+    
+    var parameterEncoding: ParameterEncoding {
+        switch self {
+            case .getPosts:
+                return URLEncoding.queryString
+        }
+    }
+    
+    var baseURL: URL {
+        return URL(string: "https://testlumi.herokuapp.com") ?? undefined("endpoint for frontend echo dose not exist")
+    }
+    
+    var headers: [String: String]? {
+        nil
+    }
+
+    var sampleData: Data {
+        Data()
+    }
+}
