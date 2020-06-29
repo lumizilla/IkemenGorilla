6bf0a041cdb69be2e9ddbdaf486dd943d365d3c1
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-29 20:52:53 +0900

    update ContestRepository

--- IkemenGorilla-iOS/Gorilla/BusinessLogic/Repositories/ContestRepository.swift
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Repositories/ContestRepository.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+
+protocol ContestRepositoryType {
+    func getContests(status: ContestStatus, page: Int) -> Single<[Contest]>
+}
+
+final class ContestRepository: ContestRepositoryType {
+    private let networkProvider: NetworkProvider<ContestTarget>
+    private let decoder = JSONDecoder()
+    
+    init(networkProvider: NetworkProvider<ContestTarget>) {
+        self.networkProvider = networkProvider
+    }
+    
+    func getContests(status: ContestStatus, page: Int) -> Single<[Contest]> {
+        networkProvider.rx.request(.getContests(status: status, page: page))
+            .filterSuccessfulStatusCodes()
+            .map([Contest].self, using: decoder)
+            .do(onError: { error in
+                logger.error(error)
+            })
+    }
+}
