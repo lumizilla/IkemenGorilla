ed448ecaaab9d62ce60a7b5fe15ba20f50fad8e2
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-30 00:56:17 +0900

    update AnimalRepository

--- IkemenGorilla-iOS/Gorilla/BusinessLogic/Repositories/AnimalRepository.swift
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Repositories/AnimalRepository.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+
+protocol AnimalRepositoryType {
+    func getPosts(animalId: String, page: Int)
+}
+
+final class AnimalRepository: AnimalRepositoryType {
+    private let networkProvider: NetworkProvider<AnimalTarget>
+    private let decoder = GorillaDecoder.default
+    
+    init(networkProvider: NetworkProvider<AnimalTarget>) {
+        self.networkProvider = networkProvider
+    }
+    
+    func getPosts(animalId: String, page: Int) {
+        networkProvider.rx.request(.getPosts(animalId: animalId, page: page))
+            .filterSuccessfulStatusCodes()
+            .map([Post].self, using: decoder)
+            .do(onError: { error in
+                logger.error(error)
+            })
+    }
+}
