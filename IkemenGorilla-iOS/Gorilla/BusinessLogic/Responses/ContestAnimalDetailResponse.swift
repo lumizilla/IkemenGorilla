4a947fd0d4fc358dee50775e0f786c29220b0a4f
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-30 00:41:11 +0900

    add codingKeys to ContestAnimalDetailResponse

--- IkemenGorilla-iOS/Gorilla/BusinessLogic/Responses/ContestAnimalDetailResponse.swift
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Responses/ContestAnimalDetailResponse.swift
@@ -17,4 +17,15 @@ struct ContestAnimalDetailResponse: Codable {
     let zooName: String
     let zooAddress: String
     let isVotedToday: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case animalId = "animal_id"
+        case animalName = "animal_name"
+        case animalIconUrl = "animal_icon_url"
+        case description
+        case zooId = "zoo_id"
+        case zooName = "zoo_name"
+        case zooAddress = "zoo_address"
+        case isVotedToday = "is_voted_today"
+    }
 }
