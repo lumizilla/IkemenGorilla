56901acbaf74ea20c51ba1a5161f5de04e403209
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-30 01:34:11 +0900

    add zooService to ServiceProvider

--- IkemenGorilla-iOS/Gorilla/BusinessLogic/Service/ServiceProvider.swift
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Service/ServiceProvider.swift
@@ -9,13 +9,16 @@
 protocol ServiceProviderType: AnyObject {
     var contestService: ContestServiceType { get set }
     var animalService: AnimalServiceType { get set }
+    var zooService: ZooServiceType { get set }
 }
 
 final class ServiceProvider: ServiceProviderType {
     
     private lazy var contestRepository = ContestRepository(networkProvider: NetworkProvider<ContestTarget>())
     private lazy var animalRepository = AnimalRepository(networkProvider: NetworkProvider<AnimalTarget>())
+    private lazy var zooRepository = ZooRepository(networkProvider: NetworkProvider<ZooTarget>())
     
     lazy var contestService: ContestServiceType = ContestService(provider: self, contestRepository: contestRepository)
     lazy var animalService: AnimalServiceType = AnimalService(provider: self, animalRepository: animalRepository)
+    lazy var zooService: ZooServiceType = ZooService(provider: self, zooRepository: zooRepository)
 }
