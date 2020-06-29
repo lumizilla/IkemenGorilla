15b2e79ff7594edf610e21413aedc209d57f7150
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-06-29 22:04:07 +0900

    update ContestDetailResponse

--- IkemenGorilla-iOS/Gorilla/BusinessLogic/Responses/ContestDetailResponse.swift
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Responses/ContestDetailResponse.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+struct ContestDetail: Codable {
+    let id: String
+    let name: String
+    let start: Date
+    let end: Date
+    let status: String
+    let catchCopy: String
+    var description: String? = ""
+    let imageUrl: String
+    let numberOfEntries: Int
+    let numberOfVotedPeople: Int
+    let numberOfVotes: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, start, end, status, description
+        case catchCopy = "catch_copy"
+        case imageUrl = "image_url"
+        case numberOfEntries = "number_of_entries"
+        case numberOfVotedPeople = "number_of_voted_people"
+        case numberOfVotes = "number_of_votes"
+    }
+}
