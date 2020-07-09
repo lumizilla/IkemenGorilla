98d550cb32a50d92c2338af85db9e2b3d570c1ae
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-07-09 16:07:05 +0900

    update ContestDetailPostReactor by adding createExplorePostDetailReactor

--- IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/Post/ContestDetailPostReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/HomeTab/ContestDetail/Post/ContestDetailPostReactor.swift
@@ -59,4 +59,9 @@ final class ContestDetailPostReactor: Reactor {
         }
         return state
     }
+    
+    func createExplorePostDetailReactor(indexPath: IndexPath) -> ExplorePostDetailReactor {
+        let posts = currentState.postCellReactors.compactMap { $0.currentState.post }
+        return ExplorePostDetailReactor(startAt: indexPath.row, posts: posts)
+    }
 }
