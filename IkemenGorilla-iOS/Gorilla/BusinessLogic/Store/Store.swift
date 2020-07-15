53c60bf5bc4f2114fcf36901e748d3f7891015b4
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-07-15 15:54:52 +0900

    update Store

--- IkemenGorilla-iOS/Gorilla/BusinessLogic/Store/Store.swift
+++ IkemenGorilla-iOS/Gorilla/BusinessLogic/Store/Store.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import SwiftyUserDefaults
+import Then
+
+protocol StoreType {
+    var userID: String? { get set }
+}
+
+final class Store: StoreType {
+    var userID: String? {
+        get { Defaults[\.userID] }
+        set { Defaults[\.userID] = newValue }
+    }
+}
