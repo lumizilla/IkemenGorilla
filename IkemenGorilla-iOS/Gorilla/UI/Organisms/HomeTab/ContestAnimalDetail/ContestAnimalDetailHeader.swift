c7260954adeff6fc604a5ef6778b40b7a82c5424
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-07-17 12:47:00 +0900

    add ContestAnimalDetailHeaderDelegate

--- IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestAnimalDetail/ContestAnimalDetailHeader.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Organisms/HomeTab/ContestAnimalDetail/ContestAnimalDetailHeader.swift
@@ -10,6 +10,10 @@ import UIKit
 import ReactorKit
 import RxSwift
 
+protocol ContestAnimalDetailHeaderDelegate {
+    func didTapZoo() -> Void
+}
+
 final class ContestAnimalDetailHeader: UIView, View, ViewConstructor {
     struct Const {
         static let imageViewHeight: CGFloat = DeviceSize.screenWidth + 88
@@ -21,6 +25,8 @@ final class ContestAnimalDetailHeader: UIView, View, ViewConstructor {
     override var intrinsicContentSize: CGSize {
         return CGSize(width: DeviceSize.screenWidth, height: Const.imageViewHeight )
     }
+    
+    var delegate: ContestAnimalDetailHeaderDelegate?
     
     // MARK: - Views
     private let imageView = UIImageView().then {
@@ -133,6 +139,27 @@ final class ContestAnimalDetailHeader: UIView, View, ViewConstructor {
         voteButton.rx.tap
             .bind { _ in
                 reactor.action.onNext(.tapVoteButton)
+            }
+            .disposed(by: disposeBag)
+        
+        zooNameLabel.rx.tapGesture()
+            .when(.recognized)
+            .bind { [weak self] _ in
+                self?.delegate?.didTapZoo()
+            }
+            .disposed(by: disposeBag)
+        
+        mapIconView.rx.tapGesture()
+            .when(.recognized)
+            .bind { [weak self] _ in
+                self?.delegate?.didTapZoo()
+            }
+            .disposed(by: disposeBag)
+        
+        addressLabel.rx.tapGesture()
+            .when(.recognized)
+            .bind { [weak self] _ in
+                self?.delegate?.didTapZoo()
             }
             .disposed(by: disposeBag)
         
