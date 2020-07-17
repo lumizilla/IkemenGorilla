f04a92cdaf624bdeeea69a5604577ab6dbd8cb2c
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-07-17 14:54:00 +0900

    update ZooAnimalListReactor

--- IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooAnimalList/ZooAnimalListReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/MapTab/ZooAnimalList/ZooAnimalListReactor.swift
@@ -11,14 +11,21 @@ import RxSwift
 
 final class ZooAnimalListReactor: Reactor {
     enum Action {
-        case loadAnimals
+        case refresh
+        case load
     }
     enum Mutation {
         case setAnimalCellReactors([ZooAnimal])
+        case addAnimalCellReactors([ZooAnimal])
+        case setPage(Int)
+        case setApiStatus(APIStatus)
     }
     struct State {
         let zoo: Zoo
         var animalCellReactors: [ZooAnimalCellReactor]
+        var page: Int = 0
+        var isReachedBottom: Bool = false
+        var apiStatus: APIStatus = .pending
         
         init(zoo: Zoo, zooAnimals: [ZooAnimal]) {
             self.zoo = zoo
@@ -36,20 +43,42 @@ final class ZooAnimalListReactor: Reactor {
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
-        case .loadAnimals:
-            return loadAnimals().map(Mutation.setAnimalCellReactors)
+        case .refresh:
+            guard currentState.apiStatus == .pending else { return .empty() }
+            return .concat(
+                .just(.setApiStatus(.refreshing)),
+                load(page: 0).map(Mutation.setAnimalCellReactors),
+                .just(.setPage(0)),
+                .just(.setApiStatus(.pending))
+            )
+        case .load:
+            guard currentState.apiStatus == .pending && !currentState.isReachedBottom else { return .empty() }
+            return .concat(
+                .just(.setApiStatus(.loading)),
+                load(page: currentState.page+1).map(Mutation.addAnimalCellReactors),
+                .just(.setPage(currentState.page+1)),
+                .just(.setApiStatus(.pending))
+            )
         }
     }
     
-    private func loadAnimals() -> Observable<[ZooAnimal]> {
-        return .just(TestData.zooAnimals(count: 8))
+    private func load(page: Int) -> Observable<[ZooAnimal]> {
+        return provider.zooService.getAnimals(zooId: currentState.zoo.id, page: page, userId: "1").asObservable()
     }
     
     func reduce(state: State, mutation: Mutation) -> State {
         var state = state
         switch mutation {
         case .setAnimalCellReactors(let zooAnimals):
+            state.animalCellReactors = zooAnimals.map { ZooAnimalCellReactor(zooAnimal: $0) }
+            state.isReachedBottom = zooAnimals.count < 8
+        case .addAnimalCellReactors(let zooAnimals):
             state.animalCellReactors += zooAnimals.map { ZooAnimalCellReactor(zooAnimal: $0) }
+            state.isReachedBottom = zooAnimals.count < 8
+        case .setPage(let page):
+            state.page = page
+        case .setApiStatus(let apiStatus):
+            state.apiStatus = apiStatus
         }
         return state
     }
