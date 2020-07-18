a1dbe992d95fe8e60c3468ecd962e44802c6139d
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-07-18 13:57:22 +0900

    update ExploreSearchResultViewController

--- IkemenGorilla-iOS/Gorilla/UI/Pages/ExploreTab/ExploreSearchResult/ExploreSearchResultViewController.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/ExploreTab/ExploreSearchResult/ExploreSearchResultViewController.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2020 admin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class ExploreSearchResultViewController: UIViewController, View, ViewConstructor {
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+    
+    // MARK: - Views
+    private let postsCollectionView = PostPhotoCollectionView(isCalculateHeight: false).then {
+        $0.contentInset.top = 8
+    }
+    
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setupViewConstraints()
+    }
+    
+    // MARK: - Setup Methods
+    func setupViews() {
+        view.addSubview(postsCollectionView)
+    }
+    
+    func setupViewConstraints() {
+        postsCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Bind Method
+    func bind(reactor: ExploreSearchResultReactor) {
+        // Action
+        
+        // State
+    }
+}
