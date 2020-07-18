1238ba69ef1871a6ecf88683dbe315a1dab73bfb
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2020-07-18 13:05:25 +0900

    add refresh and load action

--- IkemenGorilla-iOS/Gorilla/UI/Pages/ExploreTab/Explore/ExploreReactor.swift
+++ IkemenGorilla-iOS/Gorilla/UI/Pages/ExploreTab/Explore/ExploreReactor.swift
@@ -11,15 +11,22 @@ import RxSwift
 
 final class ExploreReactor: Reactor {
     enum Action {
-        case loadPosts
+        case refresh
+        case load
     }
     
     enum Mutation {
         case setPosts([Post])
+        case addPosts([Post])
+        case setPage(Int)
+        case setApiStatus(APIStatus)
     }
     
     struct State {
         var posts: [Post] = []
+        var page: Int = 0
+        var apiStatus: APIStatus = .pending
+        var didReachedBottom: Bool = false
     }
     
     var initialState: State
@@ -32,14 +39,28 @@ final class ExploreReactor: Reactor {
     
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
-        case .loadPosts:
-            return loadPosts().map(Mutation.setPosts)
+        case .refresh:
+            guard currentState.apiStatus == .pending else { return .empty() }
+            return .concat(
+                .just(.setApiStatus(.refreshing)),
+                load(page: 0).map(Mutation.setPosts),
+                .just(.setPage(0)),
+                .just(.setApiStatus(.pending))
+            )
+        case .load:
+            guard currentState.apiStatus == .pending && !currentState.didReachedBottom else { return .empty() }
+            return .concat(
+                .just(.setApiStatus(.loading)),
+                load(page: currentState.page+1).map(Mutation.addPosts),
+                .just(.setPage(currentState.page+1)),
+                .just(.setApiStatus(.pending))
+            )
         }
     }
     
-    private func loadPosts() -> Observable<[Post]> {
+    private func load(page: Int) -> Observable<[Post]> {
         logger.warning("todo: paging from ExploreReactor")
-        return provider.postService.getPosts(page: 0).asObservable()
+        return provider.postService.getPosts(page: page).asObservable()
     }
     
     func reduce(state: State, mutation: Mutation) -> State {
@@ -47,6 +68,14 @@ final class ExploreReactor: Reactor {
         switch mutation {
         case .setPosts(let posts):
             state.posts = posts
+            state.didReachedBottom = posts.count < 24
+        case .addPosts(let posts):
+            state.posts += posts
+            state.didReachedBottom = posts.count < 24
+        case .setPage(let page):
+            state.page = page
+        case .setApiStatus(let apiStatus):
+            state.apiStatus = apiStatus
         }
         return state
     }
